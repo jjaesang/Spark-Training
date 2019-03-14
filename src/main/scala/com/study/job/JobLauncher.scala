bf8faddba6ed6b90b05ce1fff84bb693df83df9b
임재상 <jaesang@zuminternet.com>
2019-03-14 17:25:24 +0900

    JobLauncher 구조화

---
+++ src/main/scala/com/study/job/JobLauncher.scala
@@ -0,0 +1,49 @@
+package com.study.job
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by jaesang on 2019-03-14.
+  */
+
+case class JobOption(
+                      name: String = "SparkBatchJob",
+                      confType: String = "db",
+                      jobId: Int = -1,
+                      confFile: String = ""
+                    )
+
+object JobLauncher extends Logger {
+
+  def parseArguments(args: Array[String]): JobOption = {
+
+    val parser = new scopt.OptionParser[JobOption]("run") {
+      // parsing arguments..
+    }
+
+    parser.parse(args, JobOption()) match {
+      case Some(o) => o
+      case None =>
+        parser.showUsage()
+        throw new IllegalArgumentException(s"failed to parse options... (${args.mkString(",")}")
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val options = parseArguments(args)
+    logger.info(s"Job Options : ${
+      options
+    }")
+
+    val spark = SparkSession
+      .builder()
+      .getOrCreate()
+
+    val job = new Job(spark, jobDescription)
+    job.run()
+
+  }
+
+}
